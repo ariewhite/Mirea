012e4821ca566084e2f471bcac281b4d3b7f6837
Murzabaev Chingiz <arie.white@bk.ru>
2022-05-14 01:56:08 +0300

    m7 part 1

---
+++ app/module7/src/main/java/com/example/module7/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.module7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //game over kill me
+}
